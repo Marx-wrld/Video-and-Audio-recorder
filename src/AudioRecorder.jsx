599272008e5d680be5bdeda7a77568b3d09491d5
Marx-wrld <iongonga@kabarak.ac.ke>
2023-02-24 12:07:55 +0300

    Audio recorder component

---
+++ src/AudioRecorder.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef } from "react";
+
+const AudioRecorder = () => {
+
+    const [permission, setPermission] = useState(false);
+    const [stream, setStream] = useState(null);
+
+    const getMicrophonePermission = async () => {
+
+        //Receiving microphone permissions using the function getMicrophonePermission
+        if ("MediaRecorder" in window){
+            try{
+                const streamData = await navigator.mediaDevices.getUserMedia(
+                //sets MediaStream received from the navigator.mediaDevices.getUserMedia function to the stream state variable
+                {
+                    audio: true,
+                    video: false,
+                });
+                setPermission(true);
+                setStream(streamData);
+            }
+            catch(err) {
+                alert(err.message);
+            }
+        }else{
+            alert("The MediaRecorder API is not supported in your browser.");
+        }
+    };
+    return (
+        <div>
+            <h2>Audio Recorder</h2>
+            <main>
+                <div className="audio-controls">
+                    {!permission ? (
+                        <button onClick={getMicrophonePermission} type="button">
+                            Get Microphone
+                        </button>
+                    ): null}
+                    {permission ? (
+                        <button type="button">
+                            Record
+                        </button>
+                    ): null}
+        </div>
+        </main>
+        </div>
+    )
+};
+export default AudioRecorder;

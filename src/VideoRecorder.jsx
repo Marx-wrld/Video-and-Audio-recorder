1a2381049872c793e8fcf50556fac056b3fb5058
Marx-wrld <iongonga@kabarak.ac.ke>
2023-02-27 14:29:05 +0300

    Video Recorder component

---
+++ src/VideoRecorder.jsx
@@ -0,0 +1,45 @@
+import { useState, useRef} from "react";
+
+const VideoRecorder = () => {
+    const [Permission, setPermission] = useState(false);
+    const [stream, setStream] = useState(null);
+
+    const getCameraPermission = async () => {
+        if ("MediarRecorder" in window) {
+            try{
+                const streamData = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: true,
+                });
+                setPermission(true);
+                setStream(streamData);
+            }catch(err) {
+                alert(err.message)
+            }
+        } else {
+            alert("The MediaRecorder API is not supported in your browser.");
+        }
+    };
+
+    return (
+        <div>
+            <h2>Video Recorder</h2>
+            <main>
+                <div className="video-controls">
+                    {!permission ? (
+                        <button onClick={getCameraPermission} type="button">
+                            Get Camera
+                        </button>
+                    ): null}
+                    {permission ? (
+                        <button type="button">
+                            Record
+                        </button>
+                    ): null}
+                </div>
+            </main>
+        </div>
+
+    );
+};
+export default VideoRecorder
